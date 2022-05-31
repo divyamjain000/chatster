66c5af050b846117ea839eec14a3f1271cf157b7
Divyam Jain <jaindivyam111@gmail.com>
2022-05-31 14:16:39 +0530

    updated

--- script/javascript.js
+++ script/javascript.js
@@ -1,24 +1,33 @@
-let a=document.getElementById("txt");
+let a=document.getElementById("txtarea");
 let c=document.getElementById("btn");
-// area.addEventListener("Keydown",myFunction);
 let b=document.getElementById("area");
-let d=document.getElementById("btn1");
-btn1.addEventListener("click",myFunction1);
+document.getElementById("area").addEventListener("keyup",function(e){
+    if(e.keyCode==13){ 
+        document.getElementById("btn").click();}});
 btn.addEventListener("click",myFunction);
 let str=b.value;
 // let arr=new array();
 // let myList=document.createElement("ul");
 // let li=document.createElement("li");
 function myFunction(event){
-    str=b.value;
-    let upper=str.toUpperCase();
+    
     // li.innerHTML=b.value;
     // myList.appendChild(li);
     // if(b.value!=null){
         // str=str+b.value+"<br>\n";
-        console.log(str);
-        a.innerHTML=upper+"<br>\n";
-        b.value=upper;
+        // a.innerHTML=str;
+        if(str=="clear()"){
+            a.innerHTML=null;
+            b.value=null;
+        }
+        else{
+
+            str=str+b.value;
+            
+            console.log(str+"<br>\n");
+            a.innerHTML=str+'\r\n';
+            b.value=null;
+        }
         // window.prompt(b.value);
     // }
     
